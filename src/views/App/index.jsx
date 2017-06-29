981dc3c7754ad1bbed47db1fe4e29de78d8c6cbf
Zachary Lee <w.zachary.lee@gmail.com>
2017-06-30 00:40:27 +0200

    whoopsies

--- src/views/App/index.jsx
+++ src/views/App/index.jsx
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
 import { Container } from 'semantic-ui-react'
 import AppHeader from './AppHeader';
-import Calendar from './Calendar'
 
 export default class App extends Component {
   render() {
     return (
       <Container text className='app'>
           <AppHeader/>
-          <Calendar/>
         </Container>
       )
   }
